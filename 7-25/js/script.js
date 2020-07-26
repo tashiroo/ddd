9c375bb8c7bb15dd8cd57e70fa43600d9d35be30
taashiroo <ing631tom@outlook.jp>
2020-07-26 23:50:32 +0900

    shuffle

---
+++ 7-25/js/script.js
@@ -0,0 +1,31 @@
+"use strict";
+
+{
+  const question = document.getElementById("question");
+  const choices = document.getElementById("choices");
+  const btn = document.getElementById("btn");
+
+  const quizSet = [
+    { q: "What is A?", c: ["A0", "A1", "a2"] },
+    { q: "What is B?", c: ["B0", "B1", "B2"] },
+    { q: "What is C?", c: ["C0", "C1", "C2"] },
+  ];
+  let currentNum = 0;
+
+  question.textContent = quizSet[currentNum].q;
+
+  function shuffle(arr) {
+    for (let i = arr.length - 1; i > 0; i--) {
+      const j = Math.floor(Math.random() * (i + 1));
+      [arr[j], arr[i]] = [arr[i], arr[j]];
+    }
+    return arr;
+  }
+
+  const shuffledChoices = shuffle([...quizSet[currentNum].c]);
+  shuffledChoices.forEach((choice) => {
+    const li = document.createElement("li");
+    li.textContent = choice;
+    choices.appendChild(li);
+  });
+}
