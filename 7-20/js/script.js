b75db5b74b06a90daf61aa2a1ae058262d7ce2f8
taashiroo <ing631tom@outlook.jp>
2020-07-31 10:20:35 +0900

    up

---
+++ 7-20/js/script.js
@@ -0,0 +1,13 @@
+"use strict";
+
+{
+  document.querySelector("button").addEventListener("click", () => {
+    const li = document.createElement("li");
+    const text = document.querySelector("input");
+    li.textContent = text.value;
+    document.querySelector("ul").appendChild(li);
+
+    text.value = "";
+    text.focus();
+  });
+}
